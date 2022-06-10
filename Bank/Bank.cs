583711362b81e061db2a7285fb885e3180f508e3
DiamondD7 <aaron.diamond2@yahoo.co.nz>
2022-06-10 19:18:46 +1200

    added a new class consisting of the banking customer details and the UI of the console

---
+++ Bank/Bank.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank
+{
+    public class Bank
+    {
+        public string CustomerName { get; set; }
+        public string CustomerId { get; set; }
+
+        public Bank(string cName, string cId)
+        {
+            CustomerName = cName;
+            CustomerId = cId;
+        }
+
+
+    }
+}
